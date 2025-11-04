72a8020cfd1e75374c024525d5ff183f041804fc
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-03 21:55:34 -0500

    Update input data and enhance debug output
    Refactor input handling and add debug prints.

--- 1.py
+++ 1.py
@@ -2,27 +2,32 @@ input = '''Vyrdax,Drakzyph,Fyrryn,Elarzris
 
 R3,L2,R3,L1'''
 
+input = '''Lornvynar,Gaereldrith,Tirnoris,Wyrlar,Rysskael,Lornselor,Silmyr,Vyrllyr,Litheth,Karthath,Craggryph,Eaddin,Nyrixsaral,Xendjor,Paldpyr,Olardra,Elarselor,Raelnarith,Norakilor,Xyrfyr
+
+L10,R9,L12,R14,L6,R11,L5,R7,L8,R7,L5,R14,L5,R12,L5,R8,L5,R9,L5,R14,L15,R12,L14,R9,L7,R12,L7,R15,L5'''
 names, _, moves = input.split('\n')
 names = names.split(',')
 moves = moves.split(',')
 name_count = len(names)
 max_idx = name_count-1
 idx = 0
+print(f'{name_count=}')
 for move in moves: 
     d = move[0]
     cnt = move[1:]
     cnt = int(cnt)
+    print(f'{move=}, {cnt=}')
+    cnt = cnt%name_count
+    print(cnt)
     if d == 'L':
         cnt = -cnt
-    cnt = cnt%name_count
     idx += cnt
-    
+    print(idx)
     if idx < 0:
         # idx = 0
         idx = name_count + cnt
     elif idx > max_idx:
         # idx = max_idx
         idx = cnt - name_count
+    print(f'{idx=}')
 print(names[idx])
-
-# not Craggryph
