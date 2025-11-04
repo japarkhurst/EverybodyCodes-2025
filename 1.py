b088f8f7222d1c76399ae06ab36d806bfc1d4273
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-03 20:17:13 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,7 +5,8 @@ R3,L2,R3,L1'''
 names, _, moves = input.split('\n')
 names = names.split(',')
 moves = moves.split(',')
-max_idx = len(names)-1
+name_count = len(names)
+max_idx = name_count-1
 idx = 0
 for move in moves: 
     d = move[0]
@@ -13,9 +14,13 @@ for move in moves:
     cnt = int(cnt)
     if d == 'L':
         cnt = -cnt
+    cnt = cnt%name_count
     idx += cnt
+    
     if idx < 0:
-        idx = 0
+        # idx = 0
+        idx = name_count + cnt
     elif idx > max_idx:
-        idx = max_idx
+        # idx = max_idx
+        idx = cnt - name_count
 print(names[idx])
