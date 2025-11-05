68f83272a674229409f75fe1fafc87a4df9e03dd
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-04 19:20:23 -0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -15,3 +15,18 @@ def divide(R1,R2):
     X2,Y2 = R2
     return (X1 // X2, Y1 // Y2)
 
+R = [0,0]
+A = [25,9]
+for I in range(3):
+    R = multiply(R,R)
+    print(R)
+    R = divide(R,(10,10))
+    print(R)
+    R = add(R,A)
+    print(R)
+
+Cycle I
+R = R * R = [0,0]
+R = R / [10,10] = [0,0]
+R = R + A = [25,9]
+
