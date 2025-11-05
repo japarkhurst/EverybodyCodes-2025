bb7acb7eb711933bdf427958ecb652a27e294383
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-04 19:15:31 -0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -3,4 +3,15 @@
 def multiply(R1,R2):
     X1,Y1 = R1
     X2,Y2 = R2
+    return (X1 * X2 - Y1 * Y2, X1 * Y2 + Y1 * X2)
+
+def add(R1,R2):
+    X1,Y1 = R1
+    X2,Y2 = R2
     return (X1 + X2, Y1 + Y2)
+
+def divide(R1,R2):
+    X1,Y1 = R1
+    X2,Y2 = R2
+    return (X1 // X2, Y1 // Y2)
+
