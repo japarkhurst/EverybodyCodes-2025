4e06a72829ebea40823096d5010e531698a26fa9
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-07 20:24:31 -0500

    Refactor number placement logic in 5.py
    Fix logic for placing numbers in the fb dictionary.

--- 5.py
+++ 5.py
@@ -2,12 +2,20 @@ input = '''58:5,3,7,8,9,10,4,5,7,8,8'''
 id,nums = input.split(':')
 l,m,r = 'l','m','r'
 fb = {}
+i = -1
 for n in nums:
     placed = False
     for i in range(len(fb)):
         if not fb[i][l] and int(n) < int(fb[i][m]):
             fb[i][l] = n
-            
+            placed = True
+        elif not fb[i][r] and int(n) > int(fb[i][m]):
+            fb[i][r] = n
+            placed = True
+    if not placed:
+        fb[i+1] = {l:None,m:n,r:None}
+spine = [fb[i][m] for i in range(len(fb))]
+print(''.join(spine))
         
     
 
