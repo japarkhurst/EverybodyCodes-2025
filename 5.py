85f0810da68d34c6e294e7a68cbcf549d53d9df6
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-07 20:49:43 -0500

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,26 +1,34 @@
-from pprint import pprint
 input = '''58:5,3,7,8,9,10,4,5,7,8,8'''
-id,nums = input.split(':')
-nums = nums.split(',')
 
+def parseLine(line):
+    id,nums = line.split(':')
+    return id,nums.split(',')
+    
 def calcQuality(nums):
-l,m,r = 'l','m','r'
-fb = {}
-i = -1
-for n in nums:
-    placed = False
-    for i in range(len(fb)):
-        if not fb[i][l] and int(n) < int(fb[i][m]):
-            fb[i][l] = n
-            placed = True
-            break
-        elif not fb[i][r] and int(n) > int(fb[i][m]):
-            fb[i][r] = n
-            placed = True
-            break
-    if not placed:
-        fb[i+1] = {l:None,m:n,r:None}
-    print(n)
-    pprint(fb)
-spine = [fb[i][m] for i in range(len(fb))]
-print(''.join(spine))
+    l,m,r = 'l','m','r'
+    fb = {}
+    i = -1
+    for n in nums:
+        placed = False
+        for i in range(len(fb)):
+            if not fb[i][l] and int(n) < int(fb[i][m]):
+                fb[i][l] = n
+                placed = True
+                break
+            elif not fb[i][r] and int(n) > int(fb[i][m]):
+                fb[i][r] = n
+                placed = True
+                break
+        if not placed:
+            fb[i+1] = {l:None,m:n,r:None}
+    spine = ''.join([fb[i][m] for i in range(len(fb))])
+    print(spine)
+    return int(spine)
+
+qualities = []
+for line in input.split('\n'):
+    id,nums = parseLine(line)
+    quality = getQuality(nums)
+    qualities.append(quality)
+print(max(qualities)-min(qualities))
+    
