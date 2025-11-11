f054a8b1cd6192ca8ef4f376a9993d04d829006e
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-11 08:29:20 -0500

    Update 6.py

--- 6.py
+++ 6.py
@@ -38,13 +38,24 @@ for idx,l in enumerate(input):
     cnt += subset_cnt
 print(cnt)
 
+input = 'AABCBABCABCabcabcABCCBAACBCa'
 cnt = 0
 multiplier = 1000
 distance = 1000
 #input *= multiplier
-input *= 3
+init_length = len(input)
+print(f'{init_length=}')
+num_needed_for_repeat = distance//init_length + 1
+input_iterations = num_needed_for_repeat * 3 # one for start, one for middle, one for end
+repeat_multiplier = multiplier - (num_needed_for_repeat*2) # number of times to multiply the middle section
+start_mid_break = num_needed_for_repeat * init_length
+mid_end_break = start_mid_break * 2
+print(f'{num_needed_for_repeat=}\n{input_iterations=}\n{repeat_multiplier=}\n{start_mid_break=}\n{mid_end_break=}')
+input *= input_iterations
 length = len(input)
+print(f'{length=}')
 start,mid,end = 0,0,0
+start_cnt, mid_cnt, end_cnt = 0,0,0
 for idx,l in enumerate(input):
     if l.isupper():
         continue
@@ -54,12 +65,17 @@ for idx,l in enumerate(input):
     #print("".join(subset))
     subset_cnt = len([x for x in subset if x == l.upper()])
     #print(subset_cnt)
-    if idx < distance:
-        start += subset_cnt
-    elif idx < length-distance:
-        mid += subset_cnt
+    if idx < start_mid_break:
+        start+=subset_cnt
+        start_cnt+=1
+    elif idx >= mid_end_break:
+        end+=subset_cnt
+        end_cnt+=1
     else:
-        end += subset_cnt
+        mid+=subset_cnt
+        mid_cnt+=1
     #cnt += subset_cnt
-cnt = start + mid*(multiplier-2) + end
+print(f'{start_cnt=},{mid_cnt=},{end_cnt=}')
+print(f'{start=},{mid=},{end=}')
+cnt = start + mid*(repeat_multiplier) + end
 print(cnt)
