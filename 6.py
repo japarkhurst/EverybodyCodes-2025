73c8aa9bb2ff5cda4d64a244221c39ba3b0ce87d
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-10 19:52:43 -0500

    Implement counting logic for letter occurrences
    Count occurrences of lowercase letters based on preceding uppercase letters in the input string.

--- 6.py
+++ 6.py
@@ -8,3 +8,17 @@ for x in input:
     elif x == 'a':
         cnt += m
 print(cnt)
+
+input = 'ABabACacBCbca'
+#input = 'ABCBABBCbbaACaACbCBbBbacBBccaAbbabCcbacAbAAACbaaACAbbbBaCBBBAccbbBaBCBAaBaCAcBcBaAAcCCCaAcbbAaCBabaA'
+input = 'ABCAbbCBAaabCAbBcBbcBCbabBCBACcCbAAcabcbCACcacBbBbBbaAcbbBCccbbBcaCCcAabABBcbBAAccabAaCcCCcCcaaCCaaAbAbcAaaAAbBbcaaAAaABaAACaCAaBabBACacCccBCcacBcccACcCaBCBbaCBabCBaACbAaABAbcCaAbcbABBbBbcabbCBbBbcBAACaABAbcCaAbbaCaAAcBCAACbBAaABBAaBacBAABACBcaccCbaBCcBcACbcBAbcCACaCbaabbBaaAAbbaAaCBBCCCaaccCAaCcBcb'
+cnt = 0
+for l in ('a','b','c'):
+    m = 0
+    subset = [x for x in input if x.lower() == l]
+    for x in input:
+        if x == l.upper():
+            m+=1
+        elif x == l:
+            cnt += m
+print(cnt)
