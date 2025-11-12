d360a338053c1bb0d94620e517426da75aefd6d8
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-12 02:09:07 +0000

    Refactor input handling and name validation logic

--- 7.py
+++ 7.py
@@ -1,33 +1,30 @@
-input = '''Oronris,Urakris,Oroneth,Uraketh
+input = '''Khara,Xaryt,Noxer,Kharax
 
-r > a,i,o
-i > p,w
-n > e,r
-o > n,m
-k > f,r
-a > k
-U > r
-e > t
-O > r
-t > h'''
-
-input = '''Harkris,Hazkris,Harox,Hazox,Kykris,Kyox,Kyagrath,Hazagrath,Haragrath
-
-K > y
-H > a
-r > a,i,o,k
+r > v,e,a,g,y
+a > e,v,x,r,g
+e > r,x,v,t
+h > a,e,v
+g > r,y
+y > p,t
+i > v,r
+K > h
+v > e
+B > r
 t > h
-z > a,o,k
-o > x
-y > a,b
-g > r
-a > g,t,b
-k > r
-i > s'''
+N > e
+p > h
+H > e
+l > t
+z > e
+X > a
+n > v
+x > z
+T > i'''
 
 names,rules = input.split("\n\n")
 names = names.split(',')
 rules = rules.split('\n')
+
 pairs = []
 pairDict = {}
 for r in rules:
@@ -36,20 +33,8 @@ for r in rules:
     pairDict[start] = ends
     for end in ends:
         pairs.append(start+end)
-print(pairDict)
-keys = list(pairDict.keys())
-name = names[0]
-unique = {name}
-for i in range(len(name)-1,11):
-    print(i)
-    subset = [x for x in unique if len(x)==i and x[-1] in keys]
-    print(subset)
-    for s in subset:
-        options = pairDict[s[-1]]
-        for opt in options:
-            unique.add(s+opt)
-unique = {u for u in unique if len(u) >= 7}
-print(len(unique))            
+#print(pairDict)
+
 
 found = False
 invalid = False
@@ -58,14 +43,26 @@ for name in names:
     for i in range(len(name)-1):
         test = name[i:i+2]
         if test not in pairs:
-            print(f'{name}: {test}')
+            #print(f'{name}: {test}')
             invalid = True
             invalidList.append(name)
             break
-    if not invalid:
-        break
-print(name)
-print([x for x in names if x not in invalidList])
-indices = [i for i,x in enumerate(names,1) if x not in invalidList]
-print(indices)
+validNames = [x for x in names if x not in invalidList]
+print(validNames)
+indices = [i for i,x in enumerate(names) if x in validNames]
 print(sum(indices))
+
+keys = list(pairDict.keys())
+names = validNames
+unique = set(names)
+for name in names:
+    for i in range(len(name)-1,11):
+        #print(i)
+        subset = [x for x in unique if len(x)==i and x[-1] in keys]
+        #print(subset)
+        for s in subset:
+            options = pairDict[s[-1]]
+            for opt in options:
+                unique.add(s+opt)
+unique = {u for u in unique if len(u) >= 7 and len(u)<=11}
+print(len(unique))            
