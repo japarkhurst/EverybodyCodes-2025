3d32bf9069314994c2eeb19ae1c4e0109d481f7e
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-12 22:46:35 -0500

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,13 +1,12 @@
 input = '1,5,2,6,8,4,1,7,3'
 nums = [int(x) for x in input.split(',')]
-cnt = 8
-crosses = sum([1 for i in range(len(nums)-1) if abs(nums[i]-nums[i+1]) == int(cnt/2)])
+nail_cnt = 8
+crosses = sum([1 for i in range(len(nums)-1) if abs(nums[i]-nums[i+1]) == int(nail_cnt/2)])
 print(crosses)
 
 
 input = '1,5,2,6,8,4,1,7,3,5,7,8,2'
 nums = [int(x) for x in input.split(',')]
-cnt = 8
 pairs = [sorted((nums[i],nums[i+1])) for i in range(len(nums)-1)]
 print(pairs)
 strung = []
