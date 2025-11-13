846771323561ed15e3cc3bd77fe7eaef1f5265bb
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-12 20:14:40 -0500

    Fix crossing count logic in 8.py

--- 8.py
+++ 8.py
@@ -1,4 +1,5 @@
 input = '1,5,2,6,8,4,1,7,3'
 nums = [int(x) for x in input.split(',')]
 cnt = 8
-crosses = sum([1 for i in range(len(nums)-1)
+crosses = sum([1 for i in range(len(nums)-1) if abs(nums[i]-nums[i+1]) == int(cnt/2)])
+print(crosses)
