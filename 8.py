8046b273ff33fa18e1521b7c82740813ff1061e7
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-12 22:08:42 -0500

    Implement knot counting logic in 8.py
    Add logic to calculate knots and max count from pairs.

--- 8.py
+++ 8.py
@@ -20,3 +20,21 @@ for s,e in pairs:
 print(knots)
 
 
+
+input = '1,5,2,6,8,4,1,7,3,6'
+nums = [int(x) for x in input.split(',')]
+cnt = 256
+pairs = [sorted((nums[i],nums[i+1])) for i in range(len(nums)-1)]
+#print(pairs)
+strung = []
+knots = 0
+max_cnt = 0
+for s,e in pairs:
+    cnt = sum(1 for s1,e1 in strung if (s<s1<e and (e1<s or e1>e)) or (s<e1<e and (s1<s or s1>e)))
+    #print(cnt)
+    max_cnt = max(cnt,max_cnt)
+    knots += cnt
+    strung.append((s,e))
+print(knots)
+print(max_cnt+1)
+
