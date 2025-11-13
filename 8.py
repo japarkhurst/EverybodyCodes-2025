6dfb0252bf307e6d9a571bf685cff31c2d0394db
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-12 22:21:56 -0500

    Update knot counting logic in 8.py
    Refactor logic to calculate maximum pairs and counts.

--- 8.py
+++ 8.py
@@ -20,21 +20,20 @@ for s,e in pairs:
 print(knots)
 
 
-
 input = '1,5,2,6,8,4,1,7,3,6'
 nums = [int(x) for x in input.split(',')]
-cnt = 256
 pairs = [sorted((nums[i],nums[i+1])) for i in range(len(nums)-1)]
-#print(pairs)
-strung = []
-knots = 0
+nail_count = 8
 max_cnt = 0
-for s,e in pairs:
-    cnt = sum(1 for s1,e1 in strung if (s<s1<e and (e1<s or e1>e)) or (s<e1<e and (s1<s or s1>e)))
-    #print(cnt)
-    max_cnt = max(cnt,max_cnt)
-    knots += cnt
-    strung.append((s,e))
-print(knots)
-print(max_cnt+1)
+for s in range(1,nail_count+1):
+    for e in range(1,nail_count+1):
+        #print((s,e))
+        cnt = sum(1 for s1,e1 in pairs if (s<s1<e and (e1<s or e1>e)) or (s<e1<e and (s1<s or s1>e)))
+        if cnt > max_cnt:
+            max_cnt = cnt
+            max_pair = [s,e]
+if max_pair in pairs:
+    max_cnt+=1
+print(max_cnt)    
 
+# not 2764
