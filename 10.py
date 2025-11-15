eba6c5c94f08f36b38b15ee62f2e688179a993ff
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-14 22:37:06 -0500

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,43 @@
+input = '''...SSS.......
+.S......S.SS.
+..S....S...S.
+..........SS.
+..SSSS...S...
+.....SS..S..S
+SS....D.S....
+S.S..S..S....
+....S.......S
+.SSS..SS.....
+.........S...
+.......S....S
+SS.....S..S..'''
+
+rows = input.split('\n')
+row_count = len(rows)
+col_count = len(rows[0]]
+                
+sheep = set()
+dragons = set()
+for y,row in enumerate(rows):
+    for x,char in enumerate(row):
+        cDict[(x,y)] = char
+        if char == 'S':
+            sheep.add((x,y))
+        elif char == 'D':
+            dragons.add((x,y))
+
+def getMoves(xy):
+    x,y = xy
+    newMoves = set()
+    newMoves.add((x+2,y+1))
+    newMoves.add((x+1,y+2))
+    newMoves.add((x-1,y+2))
+    newMoves.add((x-2,y+1))
+    newMoves.add((x-2,y-1))
+    newMoves.add((x-1,y-2))
+    newMoves.add((x+1,y-2))
+    newMoves.add((x+2,y-1))
+    return newMoves
+
+
+    
