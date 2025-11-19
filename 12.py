61f7418255da36750df92aff8f74c20c3c27fa5c
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-18 19:22:22 -0500

    Update 12.py

--- 12.py
+++ 12.py
@@ -3,3 +3,12 @@ input = '''989611
 746543
 766789'''
 
+grid = {}
+q = []
+seen = set()
+
+rows = input.split('\n')
+for y,row in enumerate(rows):
+    for x,char in enumerate(row):
+        grid[(x,y)] = int(char)
+        
