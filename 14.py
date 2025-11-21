b738c70bb068afce9b06a1d8110f25407ef4a791
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-20 22:10:17 -0500

    Update 14.py

--- 14.py
+++ 14.py
@@ -46,3 +46,66 @@ Otherwise, it remains inactive.
 '''
 
     
+input = '''#......#
+..#..#..
+.##..##.
+...##...
+...##...
+.##..##.
+..#..#..
+#......#'''
+
+target = ''.join(input.split('\n'))
+print(target)
+targetGrid = {}
+rows = input.split('\n')
+rowCount = len(rows)
+colCount = len(rows[0])
+for y,row in enumerate(rows):
+    for x,char in enumerate(row):
+        targetGrid[(x,y)] = 1 if char == '#' else 0
+print(f'{rowCount=},{colCount=}')
+
+grid = {}
+for y in range(34):
+    for x in range(34):
+        grid[(x,y)] = 0
+rowCount = 34
+colCount = 34
+def printGrid(grid,rowCount=34,colCount=34):
+    #print(grid)
+    for y in range(rowCount):
+        row = [grid[(x,y)] for x in range(colCount)]
+        print(''.join(['#' if x else '.' for x in row]))
+        
+def getNeighbors(b):
+    x,y = b
+    neighbors = [(x+1,y+1),(x-1,y-1),(x-1,y+1),(x+1,y-1)]
+    return [(x,y) for (x,y) in neighbors if 0 <= x <= colCount-1 and 0 <= y <= rowCount-1]
+
+totalActivated = 0
+for i in range(1,15000):
+    newGrid = {}
+    for g,active in grid.items():
+        neighbors = getNeighbors(g)
+        activeNeighborCount = len([g for g in neighbors if grid[g]])
+        if (active and activeNeighborCount%2 != 0) or (not active and activeNeighborCount%2 == 0):
+            newGrid[g] = 1
+        else:
+            newGrid[g] = 0
+    grid = newGrid
+    activatedCount = len([g for g in grid if grid[g]])
+    #print(activatedCount)
+    #totalActivated += activatedCount
+    #printGrid(grid)
+    subgrid = ''.join(('#' if char else '.' for (x,y),char in grid.items() if 13 <= x <= 20 and 13 <= y <= 20))
+    
+    if i in (125,1017) or subgrid == target:
+        print(f'{i}: {activatedCount}')
+        print(subgrid)
+        #print(activatedCount)
+        totalActivated += activatedCount
+        #printGrid(subgrid,rowCount=8,colCount=8)
+        #printGrid(grid)
+        
+print(totalActivated)
