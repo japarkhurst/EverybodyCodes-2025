c35559f834d23fead9df08490632e4d322345d87
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-21 22:58:00 -0500

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,5 +1,7 @@
 input = 'R3,R4,L3,L4,R3,R6,R9'
 input = 'L6,L3,L6,R3,L6,L3,L3,R6,L6,R6,L6,L6,R3,L3,L3,R3,R3,L6,L6,L3'
+#input = 'L6,L3,L6,R6,L6,L3,R6,L6,L6,R6,L3,R6,L3,L6,R3,R6,L6,L3,R6,L6,L6,R3,R3,L6,L3,R6,L3,R6,L6,L6,R3,R6,L6,L6,R6,L6,R3,L3,R6,L3,L6,R3,L3,R3,L3,R6,L6,R6,L3,L3,R6,R6,L6,L3,R3,L3,R6,L3,R3,L3'
+
 L,R = 'L','R'
 N,S,E,W = 'N','S','E','W'
 walls = []
@@ -25,23 +27,52 @@ for inst in input.split(','):
         walls.append((cx,cy))
         #print(f'Adding ({cx},{cy})')
     face = new_dir
-def printWalls(walls):
+def getNodes(walls):
     xList = sorted({w[0] for w in walls})
     yList = sorted({w[1] for w in walls})
     start,end = walls[0],walls[-1]
+    nodes = set()
     for y in range(max(yList),min(yList)-1,-1):
         row = ''
         for x in range(min(xList),max(xList)+1):
             if (x,y) == start:
                 row+='S'
+                nodes.add((x,y))
             elif (x,y) == end:
                 row+='E'
+                nodes.add((x,y))
             elif (x,y) in walls:
                 row+='#'
             else:
-                row+=' '
+                row+='.'
+                nodes.add((x,y))
         print(row)
+    return start,end,nodes
+
+def getNeighbors(b):
+    x,y = b
+    return [(x+1,y+1),(x-1,y-1),(x-1,y+1),(x+1,y-1)]
+ 
 #print(sorted(walls))
-printWalls(walls)
-                
-    
+start,end,nodes = getNodes(walls)
+#print(f'{start=},{end=},{nodes=}')
+import heapq
+distances = {n:float('inf') for n in nodes}
+distances[start]=0
+pq = [(0,start)]
+while pq:
+    c_dist,c_node = heapq.heappop(pq)
+    #if c_dist > distances[c_node]:
+        #continue
+
+    for n in getNeighbors(c_node):
+        dist = c_dist + 1
+        c_n_dist = distances.get(n)
+        print(f'{n}: {dist},{c_n_dist}')
+        if not c_n_dist:
+            continue
+        if dist < c_n_dist:
+            distances[n] = dist
+            heapq.heappush(pq,(dist,n))
+print(len(nodes))
+print(distances[end])
