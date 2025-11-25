40f10ef348f5bf24a29cbe67a4a015b5ac229c4b
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-24 19:48:28 -0500

    Update 16.py

--- 16.py
+++ 16.py
@@ -7,8 +7,17 @@ print(total)
 input = '1,2,2,2,2,3,1,2,3,3,1,3,1,2,3,2,1,4,1,3,2,2,1,3,2,2'
 wall = [int(x) for x in input.split(',')]
 wallLength = len(wall)
+nums = []
 idx = 0
 while sum(wall) > 0:
     idx+=1
     idxList = [i for i,x in enumerate(wall) if wallLength%i == 0]
-    valid = all(
+    valid = all(wall[i] >= 1 for i in idxList)
+    if not valid:
+        continue
+    for i in idxList:
+        wall[i]-=1
+    nums.append(idx)
+
+    
+    
