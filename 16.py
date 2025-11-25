85e4c908e259152c1508423302fb76cb38fa4c11
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-25 11:14:06 -0500

    Update 16.py

--- 16.py
+++ 16.py
@@ -24,13 +24,15 @@ print(total)
 print(spell)
 
 spell = '1,2,2,2,2,3,1,2,3,3,1,3,1,2,3,2,1,4,1,3,2,2,1,3,2,2'
+spell = '1,2,3,5,9'
 #spell = '1,2,4,8,12,38,66,72,98,120,122,136,154,156,163,183,202,205,218,237,248,262,269,292,300,461,601,733,887,953'
+
 spell = [int(x) for x in spell.split(',')]
         
 target = 202520252025000
 length = 94439495762954 
 target = 1000
-max_i = 1000
+max_i = 100
 maxBound = target
 minBound = 0
 i = 0
@@ -38,21 +40,23 @@ while True and i < max_i:
     i+=1
     #maxBlocks = sum(int(maxBound//num) for num in wall)
     #minBlocks = sum(int(minBound//num) for num in wall)
-    guessedLength = int((maxBound-minBound)//2)
+    guessedLength = maxBound - int((maxBound-minBound)//2)
+    print(f'Searching for {guessedLength} between {minBound} and {maxBound}')
     blocksRequired = sum(int(guessedLength//num) for num in spell)
-    print(f'{guessedLength} length uses {blocksRequired}')
+    print(f'{guessedLength} as guessed length uses {blocksRequired}')
     if blocksRequired == target:
         print(f'Target found exactly using {blocksRequired} and length {guessedLength}')
         break
     elif blocksRequired < target:
         print(f'Block count {blocksRequired} less than target using {guessedLength} length')
-        maxBound = guessedLength+1
+        minBound = guessedLength
     elif blocksRequired > target:
         print(f'Block count {blocksRequired} greater than target using {guessedLength} length')
-        minBound = guessLength-1
+        maxBound = guessedLength
     if maxBound - minBound == 1:
         print(f'Target found between {maxBound} and {minBound}; choosing {minBound}')
-
+        break
+print(minBound)  
 '''
 blocks     the length of the wall
                1                          1
