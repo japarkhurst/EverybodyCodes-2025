c88f116b7b2d1999a35a37844ea0f8ccf7c0e820
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-28 14:30:07 -0500

    Fix loop range and implement Cell dataclass
    Refactor loop range and add Cell dataclass for grid.

--- 19.py
+++ 19.py
@@ -11,11 +11,30 @@ maxHeight = 0
 for row in input.split('\n'):
     col,start,height = [int(x) for x in row.split(',')]
     window = []
-    for i in range(start,start+height+1):
+    for i in range(start,start+height):
         window.append((col,i))
     windowDict[col] = window
     width = col
     maxHeight = max(i,maxHeight)
 print(width,maxHeight)
 print(windowDict)
+
+from dataclasses import dataclass
+@dataclass
+class Cell():
+    xy: tuple(int)
+    dir: int = None
+    
+grid = {}
+for x in range(1,width+1):
+    for y in range(1,maxHeight+1):
+        if (x+y)%2 != 0:
+            continue
+        if x in windowDict:
+            window = windowDict[x]
+            for w in window:
+                grid[(w)] = Cell(w)
+        else:
+            grid[(x,y)] = Cell((x,y))
+print(grid)
 
