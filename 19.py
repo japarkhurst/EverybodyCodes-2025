fd3b0ab3069a6f15f925fe7ca270d37dac60ca1d
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-28 14:18:08 -0500

    Track width and maxHeight in window processing
    Added width and maxHeight variables to track dimensions.

--- 19.py
+++ 19.py
@@ -6,10 +6,16 @@ input = '''7,7,2
 40,8,2'''
 
 windowDict = {}
+width = 0
+maxHeight = 0
 for row in input.split('\n'):
     col,start,height = [int(x) for x in row.split(',')]
     window = []
     for i in range(start,start+height+1):
         window.append((col,i))
     windowDict[col] = window
+    width = col
+    maxHeight = max(i,maxHeight)
+print(width,maxHeight)
+print(windowDict)
 
