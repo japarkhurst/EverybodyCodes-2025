05c06221f95324e3806b740c7b3ec9c9ec8c1aa5
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-28 14:15:07 -0500

    Add windowDict creation from input data

---
+++ 19.py
@@ -0,0 +1,15 @@
+input = '''7,7,2
+12,0,4
+15,5,3
+24,1,6
+28,5,5
+40,8,2'''
+
+windowDict = {}
+for row in input.split('\n'):
+    col,start,height = [int(x) for x in row.split(',')]
+    window = []
+    for i in range(start,start+height+1):
+        window.append((col,i))
+    windowDict[col] = window
+
