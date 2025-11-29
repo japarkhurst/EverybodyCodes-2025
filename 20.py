9d8363253090fb377092e3aff5ecf99041032308
japarkhurst <63073080+japarkhurst@users.noreply.github.com>
2025-11-28 22:16:15 -0500

    Implement PriorityQueue class in 20.py
    Add a PriorityQueue class with basic heap operations.

--- 20.py
+++ 20.py
@@ -5,6 +5,26 @@ input = '''T#TTT###T##
 ....T##....
 .....#.....'''
 
+from math import inf
+from collections import defaultdict
+from dataclasses import dataclass
+import heapq
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+    def empty(self): # determine if heap is empty
+        return not self.elements
+    def put(self, item, priority): # add to heap, sorted by priority
+        heapq.heappush(self.elements, (priority, item))
+    def get(self): # return from front of heap
+        return heapq.heappop(self.elements)[1]
+    def __str__(self):
+        return f'{self.elements}'
+    def __len__(self):
+        return len(self.elements)
+
+
 tList = set()
 rows = input.split('\n')
 for y,row in enumerate(rows):
